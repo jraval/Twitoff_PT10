a0a8f94c1714fc053e8825eb78e41ba6a278a423
theraval <theraval@gmail.com>
2021-04-16 01:24:36 -0400

    made references to submodules

--- twitoff/app.py
+++ twitoff/app.py
@@ -2,10 +2,10 @@
 
 from flask import Flask, render_template, request
 import json
-from twitter import upsert_user
+from .twitter import upsert_user
 from twitoff.data_model import DB, User, Tweet
 from twitoff.Users import user_list, tweet_list
-from ml import predict_most_likely_author
+from .ml import predict_most_likely_author
 from os import path
 
 # from os import getenv
